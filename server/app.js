e1f2b5752faa5bc6b3a40f5a389ce84118323c61
WeeSi <francklucasehui@gmail.com>
2022-09-04 22:30:33 +0200

    commentary && readme

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 const corsOptions = {
     origin: [
         'http://localhost:3000',
+        'http://wecount-tennis.franckehui.fr'
     ]
 };
 
@@ -22,38 +23,59 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.post('/scores', function (req, res) {
+
+    // we take the scores from the body of the respone as it is a post request
     const scores = req.body;
-    let games = [];
-    let sets = [];
-    let user1Points = 0;
-    let user2Points = 0;
-    let user1NbGamesWin = 0;
-    let user2NbGamesWin = 0;
-    let user1NbSetsWin = 0;
-    let user2NbSetsWin = 0;
-    let game = 0;
-    let set = 0;
-    let nbPointsNecessary = 4;
 
-    scores.map((score, index,) => {
-        const currEl = score;
+    // init variables
+    let games = []; // the total of games played
+    let sets = []; // the total of sets
+    let user1Points = 0; // the user1 points of a game
+    let user2Points = 0; // the user2 points of a game
+    let user1NbGamesWin = 0; // the number of games win from user1 in a set
+    let user2NbGamesWin = 0; // the number of games win from user2 in a set
+    let user1NbSetsWin = 0; // the number of sets win from user1
+    let user2NbSetsWin = 0; // the number of sets win from user2
+    let game = 0; //the number of game
+    let set = 0; // the number of set
+    let nbPointsNecessary = 4; // the number of point necessary in a game
+    let gamesToWin = 6; // the number of game win in a set
 
+    //loop through scores
+    scores.map((score) => {
+        const currEl = score; // the current score
+
+        // add point to the user based on score id
         if (currEl.id == 1) user1Points++;
         if (currEl.id == 2) user2Points++;
 
+        // if the number of game win from user 1 and user 2 equal to 6
+        // the number of point necessary is 7
         if (user1NbGamesWin === 6 && user2NbGamesWin === 6) {
             nbPointsNecessary = 7;
         }
-
+ 
+        // if there is a difference of 2 or more and
+        // the number of point from user 1 or 2 is greater or equal to the necessary point
         if (Math.abs(user1Points - user2Points) >= 2 && (user1Points >= nbPointsNecessary || user2Points >= nbPointsNecessary)) {
-            if (!games[game]) games[game] = [];
+            // the game is win in this scope
 
+            if (!games[game]) games[game] = []; // create an array for each game
+
+            // increment the number of game win
             if (user1Points > user2Points) user1NbGamesWin++;
             if (user2Points > user1Points) user2NbGamesWin++;
 
-            if ((user1NbGamesWin > user2NbGamesWin) && (user1NbGamesWin >= 6)) user1NbSetsWin++;
-            if ((user2NbGamesWin > user1NbGamesWin) && (user2NbGamesWin >= 6)) user2NbSetsWin++;
+            // if one game win difference between the 2 players
+            if((user1NbGamesWin === 5 && user2NbGamesWin === 6) || (user1NbGamesWin === 6 && user2NbGamesWin === 5)){
+                gamesToWin = 7;
+            }
 
+            // increment the number of set win for the player
+            if ((user1NbGamesWin > user2NbGamesWin) && (user1NbGamesWin >= gamesToWin)) user1NbSetsWin++;
+            if ((user2NbGamesWin > user1NbGamesWin) && (user2NbGamesWin >= gamesToWin)) user2NbSetsWin++;
+
+            // object of the result
             games[game] = {
                 user1Points: user1Points,
                 user2Points: user2Points,
@@ -63,12 +85,31 @@ app.post('/scores', function (req, res) {
                 user2NbSetsWin
             }
 
+            // increment the game
             game++;
+
+            // add games to the sets array
             sets[set] = games;
 
-            if (user1NbGamesWin >= 6 || user2NbGamesWin >= 6) {
-                set++;
+            // if 7 game win is needed to win the set
+            if(gamesToWin === 7 && (user1NbGamesWin >= gamesToWin || user2NbGamesWin >= gamesToWin) ){
+                set++; // increment the set
+
+                // reinitialize the values
                 game = 0;
+                gamesToWin = 6;
+                games = [];
+                user2NbGamesWin = 0;
+                user1NbGamesWin = 0;
+            }
+
+            // if 6 games win is needed to win the set and there is a difference of 2 between the game win of the 2 players
+            if ((user1NbGamesWin >= gamesToWin || user2NbGamesWin >= gamesToWin) && (Math.abs(user1NbGamesWin - user2NbGamesWin)>=2) && gamesToWin === 6) {
+                set++; // increment the set
+
+                // reinitialize the values
+                game = 0;
+                gamesToWin = 6;
                 games = [];
                 user2NbGamesWin = 0;
                 user1NbGamesWin = 0;
@@ -79,24 +120,31 @@ app.post('/scores', function (req, res) {
 
         }
 
+        // if there is a difference smaller than 2 and the points of the 2 players are smaller than the necessary
         if ((user1Points - user2Points < 2) || (user1Points < nbPointsNecessary || user2Points < nbPointsNecessary)) {
             if (!games[game]) games[game] = [];
 
+            // object of the result
             games[game] = {
-                user1Points: user1Points,
-                user2Points: user2Points,
+                user1Points: user1Points, //0,15,30,40
+                user2Points: user2Points, //0,15,30,40
                 user1NbGamesWin,
                 user2NbGamesWin,
                 user1NbSetsWin,
                 user2NbSetsWin
             }
 
+            //add the games to the set
             sets[set] = games;
         }
 
+        // if there is a equality of point
         if (Math.abs((user1Points - user2Points)) == 1 && (user1Points >= nbPointsNecessary || user2Points >= nbPointsNecessary)) {
+            //Aventage of a user in this scope
+            
             if (!games[game]) games[game] = [];
 
+            // object of the result
             games[game] = {
                 user1Points: user1Points > user2Points ? 'AV' : '-',
                 user2Points: user2Points > user1Points ? 'AV' : '-',
@@ -112,6 +160,7 @@ app.post('/scores', function (req, res) {
         }
     });
 
+    //send the result
     res.send(sets);
 })
 
--- tennis/src/App.js
+++ tennis/src/App.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 
+// prevent characters in input type number
 const exceptions = ["e", "E", "+", "-", "."];
-
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -15,30 +15,41 @@ const App = () => {
   const usertwolevel = useRef();
 
 
+  // add users datas in state
   const setUsersData = () => {
     const user1 = {
       name: userone.current.value,
-      pct: useronelevel.current.value * 10,
+      pct: useronelevel.current.value * 10, // level of user transformed in percentage
       id: 1
     }
 
     const user2 = {
       name: usertwo.current.value,
-      pct: usertwolevel.current.value * 10,
+      pct: usertwolevel.current.value * 10, // level of user transformed in percentage
       id: 2
     }
 
+    // add 2 users
     setUsers([user1, user2]);
   }
 
+  // each click, a point is added
   const addPoint = () => {
+
+    // we expand the number of user1 and user2 based on their pct values
+    // if pct of user1 is 90, there will be 90 user1
+    // same for user2
     const expanded = users.flatMap(user => Array(user.pct).fill(user));
+    // take the winner randomly
     const winner = expanded[Math.floor(Math.random() * expanded.length)];
 
+    // add the winner to points
     setPoints(points => [...points, winner]);
   }
 
   const checkScores = async () => {
+
+    // config for sending request to the backend
     const myInit = {
       method: 'POST',
       body: JSON.stringify(points),
@@ -46,85 +57,41 @@ const App = () => {
         "Content-Type": "application/json"
       }
     };
-    const data = await fetch('http://localhost:8000/scores', myInit);
+
+    const data = await fetch('http://wecount-tennis-api.franckehui.fr/scores', myInit);
+
+    // transform the data to json
     const response = await data.json();
     setSets(response);
   }
 
+  // as I modeled the data, the current game is in the last set and the last game played 
   const lastSet = sets[sets.length - 1];
-  if(lastSet) var currentGame = lastSet[lastSet.length - 1];
+  if (lastSet) var currentGame = lastSet[lastSet.length - 1];
 
-  if(currentGame) var gameFinished = currentGame.user1NbSetsWin === 3 || currentGame.user2NbSetsWin === 3;
+  // if user1 or user2 has 3 sets win, the game is finished
+  if (currentGame) var gameFinished = currentGame.user1NbSetsWin === 3 || currentGame.user2NbSetsWin === 3;
 
-  console.log(gameFinished);
 
   return (
     <div className="App">
 
-      {users.length > 0 && <>
-        <div>{users[0].name} : {users[0].pct}</div>
-        <div>{users[1].name} : {users[1].pct}</div>
-      </>}
+      <UsersInfo users={users} />
 
+      <Form
+        setUsersData={setUsersData}
+        userone={userone}
+        useronelevel={useronelevel}
+        usertwo={usertwo}
+        usertwolevel={usertwolevel}
+        users={users}
+      />
 
-      {!users.length && <>
-        <div>
-          <input ref={userone} name="user1" placeholder="Utilisateur 1" />
-          <input onKeyDown={(event) => exceptions.includes(event.key) && event.preventDefault()} ref={useronelevel} name="user1level" placeholder="Niveau" type={"number"} max="10" min="1" />
-        </div>
+      <Button text={"Ajouter un point"} action={addPoint} disabled={gameFinished} />
 
-        <div style={{ marginTop: "20px" }}>
-          <input ref={usertwo} name="user2" placeholder="Utilisateur 2" />
-          <input onKeyDown={(event) => exceptions.includes(event.key) && event.preventDefault()} ref={usertwolevel} name="user2level" placeholder="Niveau" type={"number"} max="10" min="1" />
-        </div>
+      <Table users={users} sets={sets} currentGame={currentGame} />
 
-
-        <div style={{ marginTop: "20px" }}>
-          <button onClick={setUsersData}>Go</button>
-        </div>
-      </>}
-
-      <div style={{ marginTop: "20px" }}>
-        <button disabled={gameFinished} onClick={addPoint}>add point</button>
-      </div>
-
-      <div style={{ marginTop: "20px" }}>
-        <button onClick={checkScores}>Scores</button>
-      </div>
-
-      {sets.length > 0 && <>
-      
-      {(currentGame.user1NbSetsWin < 3 && currentGame.user2NbSetsWin < 3) && <span>Jeu en cours, pas de vainqueur</span> }
-      {currentGame.user1NbSetsWin === 3 && <span>Jeu fini, Vainqueur: {users[0].name} {users[0].pct} </span>}
-      {currentGame.user2NbSetsWin === 3 && <span>Jeu fini, Vainqueur: {users[1].name} {users[1].pct}</span>}
-
-        <table>
-          <tr>
-            <td></td>
-            {sets.map((el, index) => {
-              return <th key={`set ${index + 1}`}>Set {index + 1}</th>
-            })}
-            <th>Current Game</th>
-          </tr>
-          <tr>
-            <th scope="row">{users[0].name}</th>
-            {sets.map((el, index) => {
-              return <td>{el[el.length - 1].user1NbGamesWin}</td>
-            })}
-            <td><PointConverter point={currentGame?.user1Points || 0} /></td>
-
-          </tr>
-          <tr>
-            <th scope="row">{users[1].name}</th>
-            {sets.map((el, index) => {
-              return <td key={el[el.length - 1].user2Points}>{el[el.length - 1].user2NbGamesWin}</td>
-            })}
-            <td>
-              <td><PointConverter point={currentGame?.user2Points || 0} /></td>
-            </td>
-          </tr>
-        </table>
-      </>}
+      <Button text={"Vérifier le score"} action={checkScores} />
 
       <RecapPoints points={points} />
 
@@ -132,8 +99,44 @@ const App = () => {
   );
 }
 
+const UsersInfo = ({ users }) => {
+  return (
+    <>
+      {users.length > 0 && <>
+        <div>{users[0].name} : {users[0].pct}</div>
+        <div>{users[1].name} : {users[1].pct}</div>
+      </>}
+    </>
+  )
+}
+const Form = ({ users, userone, useronelevel, usertwo, usertwolevel, setUsersData }) => {
+
+  const keydown = (evt) => {
+    exceptions.includes(evt.key) && evt.preventDefault();
+  }
+  
+  return (
+    <>
+      {!users.length && <>
+        <div>
+          <input ref={userone} name="user1" placeholder="Utilisateur 1" />
+          <input onKeyDown={(event) => keydown(event)} ref={useronelevel} name="user1level" placeholder="Niveau" type={"number"} max="10" min="1" />
+        </div>
+
+        <div style={{ marginTop: "20px" }}>
+          <input ref={usertwo} name="user2" placeholder="Utilisateur 2" />
+          <input onKeyDown={(event) => keydown(event)} ref={usertwolevel} name="user2level" placeholder="Niveau" type={"number"} max="10" min="1" />
+        </div>
+
+        <div style={{ marginTop: "20px" }}>
+          <button onClick={setUsersData}>Go</button>
+        </div>
+      </>}
+    </>
+  )
+}
 const RecapPoints = ({ points }) => {
-  return (<div style={{ position: "absolute", right: "10px", top: "10px", maxHeight: "200px", overflowY:"auto" }}>
+  return (<div style={{ position: "absolute", right: "10px", top: "10px", maxHeight: "200px", overflowY: "auto" }}>
     {points.length > 0 &&
       <>
         {points.map((point, index) => {
@@ -144,7 +147,6 @@ const RecapPoints = ({ points }) => {
   </div>
   )
 }
-
 const PointConverter = ({ point }) => {
   switch (point) {
     case 0: return 0;
@@ -161,6 +163,52 @@ const PointConverter = ({ point }) => {
 
     default: return 40;
   }
+}
+const Button = ({ text, action, disabled }) => {
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <button disabled={disabled} onClick={action}>{text}</button>
+    </div>
+  )
+}
+const Table = ({ sets, currentGame, users }) => {
+  return (
+    <>
+      {sets.length > 0 && <>
+
+        {(currentGame.user1NbSetsWin < 3 && currentGame.user2NbSetsWin < 3) && <span>Jeu en cours, pas de vainqueur</span>}
+        {currentGame.user1NbSetsWin === 3 && <span>Jeu fini, Vainqueur: {users[0].name} {users[0].pct} </span>}
+        {currentGame.user2NbSetsWin === 3 && <span>Jeu fini, Vainqueur: {users[1].name} {users[1].pct}</span>}
+
+        <table>
+          <tr>
+            <td></td>
+            {sets.map((el, index) => {
+              return <th key={`set ${index + 1}`}>Set {index + 1}</th>
+            })}
+            <th>Current Game</th>
+          </tr>
+          <tr>
+            <th scope="row">{users[0].name}</th>
+            {sets.map((el, index) => {
+              return <td key={index + el[el.length - 1].user1NbGamesWin}>{el[el.length - 1].user1NbGamesWin}</td>
+            })}
+            <td><PointConverter point={currentGame?.user1Points || 0} /></td>
+
+          </tr>
+          <tr>
+            <th scope="row">{users[1].name}</th>
+            {sets.map((el, index) => {
+              return <td key={el[el.length - 1].user2Points + index}>{el[el.length - 1].user2NbGamesWin}</td>
+            })}
+            <td>
+              <td><PointConverter point={currentGame?.user2Points || 0} /></td>
+            </td>
+          </tr>
+        </table>
+      </>}
+    </>
+  )
 }
 
 export default App;
